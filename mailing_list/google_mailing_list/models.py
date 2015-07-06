f11faf01b6633b9edab1abffec647c81435b37fa
Chelsea Overstreet <overstreetchelsea@gmail.com>
2015-07-06 16:57:32 -0400

    Google Groups and Django- WIP

--- mailing_list/google_mailing_list/models.py
+++ mailing_list/google_mailing_list/models.py
@@ -4,6 +4,8 @@ from oauth2client.file import Storage
 from oauth2client.django_orm import FlowField
 from oauth2client.django_orm import CredentialsField
 from django.http import HttpRequest
+import urllib
+import urllib2
 import os
 import logging
 from httplib2 import Http
@@ -18,12 +20,7 @@ from oauth2client.client import flow_from_clientsecrets
 from oauth2client.django_orm import Storage
 from django.conf import settings
 from django.contrib.auth.models import User
-from apiclient.discovery import build
-from oauth2client.client import SignedJwtAssertionCredentials
 
-class FlowModel(models.Model):
-  id = models.ForeignKey(User, primary_key=True)
-  flow = FlowField()
 
 class CredentialsModel(models.Model):
     id = models.ForeignKey(User, primary_key=True)
@@ -36,43 +33,39 @@ class SendData(models.Model):
     def send_data(self, request):
         name = self.name
         email = self.email
-        # user = request.user
+        user = self.id
         final = "https://www.googleapis.com/admin/directory/v1/groups"
         scope = "https://www.googleapis.com/auth/admin.directory.group"
-        client_secrets = os.path.join(os.path.dirname(__file__), '..', 'client_secrets.json')
-        client_email="578636167063-78ajvmmfntd9c7iokk0gatqbuspu7rd6@developer.gserviceaccount.com"
+        
 
-        with open("client_privacy.p12") as f:
-            private_key = f.read()
-
-        credentials = SignedJwtAssertionCredentials(client_email, private_key,
-            'https://www.googleapis.com/auth/sqlservice.admin')
-
-        flow = flow_from_clientsecrets(client_secrets,
+        flow = OAuth2WebServerFlow(client_id,
+                                   client_secret,
                            scope=scope,
                            redirect_uri='localhost')
 
+        storage = Storage(CredentialsModel, 'id', user, 'credential')
+        credential = storage.get()
         http = Http()
-        credentials.authorize(http)
-        groups = build("directory", "v1", http=http)
-
-        # storage = Storage(CredentialsModel, 'id', user, 'credential')
-        # credential = storage.get()
+        credential.authorize(http)
         post_data = {
             "email": email,
             "name": name
         }
-        # if credential is None or credential.invalid == True:
-        #     flow.params['state'] = xsrfutil.generate_token(settings.SECRET_KEY,
-        #                                                    user)
-        #     authorize_url = flow.step1_get_authorize_url()
-        #     authorize_url = HttpResponseRedirect(authorize_url)
-        #     credential = flow.step2_exchange(authorize_url)
-        #     storage = Storage(CredentialsModel, 'id', user, 'credential')
-        #     storage.put(credential)
-        # credential = flow.step2_exchange(credential)
-        # result = credential.authorize(Http())
-        return result
+
+        if credential is None or credential.invalid == True:
+            flow.params['state'] = xsrfutil.generate_token(settings.SECRET_KEY,
+                                                           user)
+            authorize_url = flow.step1_get_authorize_url()
+            authorize_url = HttpResponseRedirect(authorize_url)
+            credential = flow.step2_exchange(authorize_url)
+            storage = Storage(CredentialsModel, 'id', user, 'credential')
+            storage.put(credential)
+        credential = flow.step2_exchange(credential)
+        post_data=urllib.urlencode(post_data)
+        result = credential.authorize(urllib2.Request(final, post_data))
+        response = urllib2.urlopen(result)
+        the_page = response.read()
+        return the_page
 
 
 class MailingList(models.Model):
